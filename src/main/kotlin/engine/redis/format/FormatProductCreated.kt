224c35faf8df5eb265a579929b338b7a89654a59
Mateo Serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-11-06 00:59:28 -0300

    fix: finally, recieve the string and pass it to json, kinda ugly but there is no other way

--- src/main/kotlin/engine/redis/format/FormatProductCreated.kt
+++ src/main/kotlin/engine/redis/format/FormatProductCreated.kt
@@ -1,11 +1,10 @@
 package engine.redis.format
 
 import com.fasterxml.jackson.databind.JsonNode
-import factory.Version
 
 data class FormatProductCreated(
     val language: String,
-    val version: Version,
+    val version: String,
     val config: JsonNode,
     val assetPath: String,
 )
