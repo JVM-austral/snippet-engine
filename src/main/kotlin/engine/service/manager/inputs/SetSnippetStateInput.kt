ed9e9e1dadf9fb74b533ef8bf84df74a1316e123
VitoContestabile <130693522+VitoContestabile@users.noreply.github.com>
2025-11-15 00:25:06 -0300

    Revert "feat: reddis linter adapted"

--- src/main/kotlin/engine/service/manager/inputs/SetSnippetStateInput.kt
+++
@@ -1,9 +0,0 @@
-package engine.service.manager.inputs
-
-import jakarta.validation.constraints.NotNull
-import org.hibernate.validator.constraints.UUID
-
-data class SetSnippetStateInput (
-    val state: CompilantState,
-    val snippetId: String,
-)
